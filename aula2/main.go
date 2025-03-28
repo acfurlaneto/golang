3298261dcf787f54c04b5efbd6ffa4277e2a7a3b
Ana <ana.furlaneto@aluno.senai.br>
2025-03-28 08:31:11 -0300

    programa

--- aula2/main.go
+++ aula2/main.go
@@ -2,17 +2,21 @@ package main
 
 import "fmt"
 
-func main(){
-	a, b := 10, 3
-	fmt.Println("A soma é:", a + b)
-	fmt.Println("A subtração é:", a - b)
-	fmt.Println("A divisão é:", a / b)
-	fmt.Println("A multiplicação é:", a * b)
-	fmt.Println("O resto da divisão é:", a % b)
-	a+=1
-	fmt.Println("Incrementar a", a)
+func main() {
+    var num1, num2 float64
 
-	if a > 0 && b > 0 {
-		fmt.Println("Números Positivos")
-	}
+    fmt.Print("Digite o primeiro número: ")
+    fmt.Scan(&num1)
+    fmt.Print("Digite o segundo número: ")
+    fmt.Scan(&num2)
+
+    fmt.Println("Soma:", num1+num2)
+    fmt.Println("Subtração:", num1-num2)
+    fmt.Println("Multiplicação:", num1*num2)
+
+    if num2 != 0 {
+        fmt.Println("Divisão:", num1/num2)
+    } else {
+        fmt.Println("Não é possível dividir por zero.")
+    }
 }
