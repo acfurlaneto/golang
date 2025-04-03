92bdcbe83bded70556d072395473fa2ff9d27889
Ana <ana.furlaneto@aluno.senai.br>
2025-04-03 09:19:57 -0300

    vetores

--- aula2/main.go
+++ aula2/main.go
@@ -9,4 +9,21 @@ func main() {
     fmt.Println(nomes)
     nomes[3] = "Clark Kent"
     fmt.Println(nomes)
+    // Slice 
+    var score = []int{100, 200, 300, 400}
+    fmt.Println(score)
+    score[1] = 2
+    fmt.Println(score, len(score), cap(score))
+    rangeOne := score[1:3]
+    fmt.Println(rangeOne)
+    rangeTwo := score[2:]
+    fmt.Println(rangeTwo)
+    rangeThree := score[:3]
+    fmt.Println(rangeThree)
+
+    var superherois = []string{"Deadpool", "Homem-aranha", "Motoqueiro Fantasma"}
+    fmt.Println(superherois)
+    superherois = append(superherois, "Ben 10", "Batman")
+    fmt.Println(superherois, len(superherois), cap(superherois))
+    
 }
